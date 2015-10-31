d5da942cb1d0bd9b9c031047e5f470586f2468c2
ptrost <info@cynova.net>
2015-10-31 13:26:40 +0100

    Read GPS data from JPEG images

--- src/AppBundle/Services/MetadataReader/JPEGMetadataReader.php
+++ src/AppBundle/Services/MetadataReader/JPEGMetadataReader.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace AppBundle\Services\MetadataReader;
 
@@ -6,78 +6,135 @@ use Symfony\Component\HttpFoundation\File\File;
 
 class JPEGMetadataReader implements MetadataReader
 {
-    
     const EXIF_ORIENTATION_TOP_LEFT_SIDE = 1;
-    
+
     const EXIF_ORIENTATION_TOP_RIGHT_SIDE = 2;
-    
+
     const EXIF_ORIENTATION_BOTTOM_RIGHT_SIDE = 3;
-    
+
     const EXIF_ORIENTATION_BOTTOM_LEFT_SIDE = 4;
-    
+
     const EXIF_ORIENTATION_LEFT_SIDE_TOP = 5;
-    
+
     const EXIF_ORIENTATION_RIGHT_SIDE_TOP = 6;
-    
+
     const EXIF_ORIENTATION_RIGHT_SIDE_BOTTOM = 7;
-    
+
     const EXIF_ORIENTATION_LEFT_SIDE_BOTTOM = 8;
-    
+
     /**
-     * @param File $file 
+     * @param File $file
+     *
      * @return array
      */
-    public function read(File $file) 
+    public function read(File $file)
     {
         $exiftags = exif_read_data($file->getPathname());
         $metadata = [];
 
-        if (isset($exiftags['FileSize'])) { 
-            $metadata['filesize'] = $exiftags['FileSize']; 
+        if (isset($exiftags['FileSize'])) {
+            $metadata['filesize'] = $exiftags['FileSize'];
         }
-        
-        if (isset($exif['GPSLongitude']) && isset($exif['GPSLongitudeRef']) && isset($exif['GPSLatitude']) && isset($exif['GPSLatitudeRef'])) {
-            $metadata['longitude'] = $this->getGps($exif['GPSLongitude'], $exif['GPSLongitudeRef']);
-            $metadata['latitude'] = $this->getGps($exif['GPSLatitude'], $exif['GPSLatitudeRef']);
-        }
-       
-        if (isset($exiftags['Orientation']) 
-            && in_array($exiftags['Orientation'], [self::EXIF_ORIENTATION_LEFT_SIDE_TOP, self::EXIF_ORIENTATION_RIGHT_SIDE_TOP, self::EXIF_ORIENTATION_RIGHT_SIDE_BOTTOM, self::EXIF_ORIENTATION_LEFT_SIDE_BOTTOM])) {
-            if (isset($exiftags['COMPUTED']) && isset($exiftags['COMPUTED']['Width'])) {
-                $metadata['height'] = $exiftags['COMPUTED']['Width']; 
-            }
-            if (isset($exiftags['COMPUTED']) && isset($exiftags['COMPUTED']['Height'])) {
-                $metadata['width'] = $exiftags['COMPUTED']['Height']; 
-            }
-        } else {
-            if (isset($exiftags['COMPUTED']) && isset($exiftags['COMPUTED']['Width'])) {
-                $metadata['width'] = $exiftags['COMPUTED']['Width']; 
-            }
-            if (isset($exiftags['COMPUTED']) && isset($exiftags['COMPUTED']['Height'])) {
-                $metadata['height'] = $exiftags['COMPUTED']['Height']; 
-            }
-        }
-        
+
+        $metadata = array_merge($metadata, $this->getGPSFromExifData($exiftags));
+        $metadata = array_merge($metadata, $this->getDimensionsFromExifData($exiftags));
+
         return $metadata;
-    }   
-    
+    }
+
     /**
-     * Helper function to extract and format GPS coordinates from EXIF data
+     * @param array $data
+     *
+     * @return array
      */
-    protected function getGps($exifCoord, $hemi) 
+    private function getGPSFromExifData(array $data)
     {
-        $degrees = count($exifCoord) > 0 ? $this->gps2Num($exifCoord[0]) : 0;
-        $minutes = count($exifCoord) > 1 ? $this->gps2Num($exifCoord[1]) : 0;
-        $seconds = count($exifCoord) > 2 ? $this->gps2Num($exifCoord[2]) : 0;
+        $metadata = [];
+        if (isset($data['GPSLongitude']) && isset($data['GPSLongitudeRef'])
+            && isset($data['GPSLatitude']) && isset($data['GPSLatitudeRef'])) {
+            $metadata['longitude'] = $this->getGps($data['GPSLongitude'], $data['GPSLongitudeRef']);
+            $metadata['latitude'] = $this->getGps($data['GPSLatitude'], $data['GPSLatitudeRef']);
+        }
+
+        return $metadata;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    private function getDimensionsFromExifData(array $data)
+    {
+        if (!isset($data['COMPUTED'])) {
+            return[];
+        }
+
+        $computed = $data['COMPUTED'];
+        if (!isset($computed['Width']) || !isset($computed['Height'])) {
+            return[];
+        }
+
+        if (isset($data['Orientation'])
+            && in_array($data['Orientation'], [
+                self::EXIF_ORIENTATION_LEFT_SIDE_TOP,
+                self::EXIF_ORIENTATION_RIGHT_SIDE_TOP,
+                self::EXIF_ORIENTATION_RIGHT_SIDE_BOTTOM,
+                self::EXIF_ORIENTATION_LEFT_SIDE_BOTTOM, ])) {
+            $metadata = [
+                'height' => $computed['Width'],
+                'width' => $computed['Height'],
+            ];
+
+            return $metadata;
+        }
+
+        $metadata = [
+            'height' => $computed['Height'],
+            'width' => $computed['Width'],
+        ];
+
+        return $metadata;
+    }
+
+    /**
+     * Helper Function to extract and format GPS coordinates from EXIF data.
+     *
+     * @param $exifCoord
+     * @param $hemi
+     *
+     * @return int
+     */
+    protected function getGps($exifCoord, $hemi)
+    {
+        $degrees = 0;
+        if (count($exifCoord) > 0) {
+            $degrees = $this->gps2Num($exifCoord[0]);
+        }
+
+        $minutes = 0;
+        if (count($exifCoord) > 1) {
+            $minutes = $this->gps2Num($exifCoord[1]);
+        }
+
+        $seconds = 0;
+        if (count($exifCoord) > 2) {
+            $seconds = $this->gps2Num($exifCoord[2]);
+        }
+
         $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
 
         return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
     }
 
     /**
-     * Helper function to extract and format GPS coordinates from EXIF data
+     * Helper function to extract and format GPS coordinates from EXIF data.
+     *
+     * @param $coordPart
+     *
+     * @return float|int
      */
-    protected function gps2Num($coordPart) 
+    protected function gps2Num($coordPart)
     {
         $parts = explode('/', $coordPart);
         if (count($parts) <= 0) {
--- src/AppBundle/Tests/Services/MetadataReader/JPEGMetadataReaderTest.php
+++ src/AppBundle/Tests/Services/MetadataReader/JPEGMetadataReaderTest.php
@@ -17,4 +17,24 @@ class JPEGMetadataReaderTest extends BaseWebTestCase
         $this->assertEquals(640, $result['width']);
         $this->assertEquals(480, $result['height']);
     }
+    
+    public function testReadGPS() 
+    {
+        $reader = $this->getContainer()->get('tb.media.metadata.jpeg');
+        $file = new File(realpath(__DIR__ . '/../../../DataFixtures/Media/test_gps.jpg'));
+        
+        $result = $reader->read($file);
+        $this->assertEquals(-3.50391944444, $result['longitude']);
+        $this->assertEquals(55.5488555556, $result['latitude']);
+    }
+    
+    public function testReadDimensions() 
+    {
+        $reader = $this->getContainer()->get('tb.media.metadata.jpeg');
+        $file = new File(realpath(__DIR__ . '/../../../DataFixtures/Media/test_dimensions.jpg'));
+        
+        $result = $reader->read($file);
+        $this->assertEquals(3456, $result['width']);
+        $this->assertEquals(4608, $result['height']);
+    }
 }
