004477b73797e7dfc18a3169c67bdd6299bcc5b0
ptrost <info@cynova.net>
2016-06-20 17:10:44 +0200

    Support setting elasticsearch host from heroku config

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,7 +1,11 @@
 <?php 
 
 foreach ($_SERVER as $key => $value) {
-    if (strpos($key, 'SYMFONY__') === 0) {
+    if ($key === 'SYMFONY__ELASTICSEARCH_HOSTS') {
+        $config = $container->getParameter('elasticsearch');
+        $config['hosts'] = [$value];
+        $container->setParameter('elasticsearch', $config);
+    } elseif (strpos($key, 'SYMFONY__') === 0) {
         $paramName = strtolower(substr($key, 9));
         $container->setParameter($paramName, $value);
     }
