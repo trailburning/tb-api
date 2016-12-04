463c0d899b07a6b943952358fa0cb5579c1838d9
ptrost <info@cynova.net>
2016-12-04 22:14:12 +0100

    removed useless status endpoint

--- tests/AppBundle/Controller/StatusControllerTest.php
+++ tests/AppBundle/Controller/StatusControllerTest.php
@@ -6,11 +6,19 @@ use Tests\AppBundle\BaseWebTestCase;
 
 class StatusControllerTest extends BaseWebTestCase
 {
-    public function testGetAction()
+//    public function testGetAction()
+//    {
+//        $client = $this->makeClient();
+//
+//        $client->request('GET', '/v2');
+//        $this->assertJsonResponse($client->getResponse(), 200);
+//    }
+
+    public function testGetSummaryAction()
     {
         $client = $this->makeClient();
 
-        $client->request('GET', '/v2/');
+        $client->request('GET', '/v2/summary');
         $this->assertJsonResponse($client->getResponse(), 200);
     }
 }
